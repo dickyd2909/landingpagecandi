37d45f2e9765387ddd39cb084a05d3efa7a9fb56
Dicky Darmawan <63191799+dickyd2909@users.noreply.github.com>
2023-06-07 14:57:14 +0700

    membuat wave path

--- apps/view/slideshow.php
+++ apps/view/slideshow.php
@@ -1,115 +1,131 @@
 <div id="bgslideshow">
-	<script src="assets/js/jssor.slider-23.1.0.min.js"></script>
-	<script type="text/javascript">
-		window.jssor_1_slider_init = function() {
-
-			var jssor_1_SlideshowTransitions = [
-			{$Duration:3000,$Zoom:11,$Easing:{$Zoom:$Jease$.$InCubic,$Opacity:$Jease$.$OutQuad},$Opacity:2}
-			];
-
-			var jssor_1_options = {
-			$AutoPlay: 1,
-			$SlideshowOptions: {
-				$Class: $JssorSlideshowRunner$,
-				$Transitions: jssor_1_SlideshowTransitions
-			},
-			$ArrowNavigatorOptions: {
-				$Class: $JssorArrowNavigator$,
-				$ChanceToShow: 1
-			},
-			$BulletNavigatorOptions: {
-				$Class: $JssorBulletNavigator$,
-				$SpacingX: 20,
-				$SpacingY: 20,
-				$ChanceToShow: 1
-			}
+	<script src="assets/js/jssor.js"></script>
+	<script src="assets/js/jssor.slider.js"></script>
+	<script>
+		jQuery(document).ready(function ($) {
+			var _CaptionTransitions = [];
+			_CaptionTransitions["L"] = { $Duration: 900, x: 0.6, $Easing: { $Left: $JssorEasing$.$EaseInOutSine }, $Opacity: 2 };
+			_CaptionTransitions["R"] = { $Duration: 900, x: -0.6, $Easing: { $Left: $JssorEasing$.$EaseInOutSine }, $Opacity: 2 };
+			_CaptionTransitions["T"] = { $Duration: 900, y: 0.6, $Easing: { $Top: $JssorEasing$.$EaseInOutSine }, $Opacity: 2 };
+			_CaptionTransitions["B"] = { $Duration: 900, y: -0.6, $Easing: { $Top: $JssorEasing$.$EaseInOutSine }, $Opacity: 2 };
+			_CaptionTransitions["ZMF|10"] = { $Duration: 900, $Zoom: 11, $Easing: { $Zoom: $JssorEasing$.$EaseOutQuad, $Opacity: $JssorEasing$.$EaseLinear }, $Opacity: 2 };
+			_CaptionTransitions["RTT|10"] = { $Duration: 900, $Zoom: 11, $Rotate: 1, $Easing: { $Zoom: $JssorEasing$.$EaseOutQuad, $Opacity: $JssorEasing$.$EaseLinear, $Rotate: $JssorEasing$.$EaseInExpo }, $Opacity: 2, $Round: { $Rotate: 0.8} };
+			_CaptionTransitions["RTT|2"] = { $Duration: 900, $Zoom: 3, $Rotate: 1, $Easing: { $Zoom: $JssorEasing$.$EaseInQuad, $Opacity: $JssorEasing$.$EaseLinear, $Rotate: $JssorEasing$.$EaseInQuad }, $Opacity: 2, $Round: { $Rotate: 0.5} };
+			_CaptionTransitions["RTTL|BR"] = { $Duration: 900, x: -0.6, y: -0.6, $Zoom: 11, $Rotate: 1, $Easing: { $Left: $JssorEasing$.$EaseInCubic, $Top: $JssorEasing$.$EaseInCubic, $Zoom: $JssorEasing$.$EaseInCubic, $Opacity: $JssorEasing$.$EaseLinear, $Rotate: $JssorEasing$.$EaseInCubic }, $Opacity: 2, $Round: { $Rotate: 0.8} };
+			_CaptionTransitions["CLIP|LR"] = { $Duration: 900, $Clip: 15, $Easing: { $Clip: $JssorEasing$.$EaseInOutCubic }, $Opacity: 2 };
+			_CaptionTransitions["MCLIP|L"] = { $Duration: 900, $Clip: 1, $Move: true, $Easing: { $Clip: $JssorEasing$.$EaseInOutCubic} };
+			_CaptionTransitions["MCLIP|R"] = { $Duration: 900, $Clip: 2, $Move: true, $Easing: { $Clip: $JssorEasing$.$EaseInOutCubic} };
+			
+			var options = {
+				$FillMode: 2, 
+				$AutoPlay: true,
+				$AutoPlayInterval: 4000,
+				$PauseOnHover: 1,			
+				$ArrowKeyNavigation: true,
+				$SlideEasing: $JssorEasing$.$EaseOutQuint,
+				$SlideDuration: 800,
+				$MinDragOffsetToSlide: 20,
+				//$SlideWidth: 600,
+				//$SlideHeight: 300,
+				$SlideSpacing: 0,
+				$DisplayPieces: 1,
+				$ParkingPosition: 0,
+				$UISearchMode: 1,
+				$PlayOrientation: 1,
+				$DragOrientation: 1,
+				
+				$CaptionSliderOptions: {
+					$Class: $JssorCaptionSlider$,
+					$CaptionTransitions: _CaptionTransitions,
+					$PlayInMode: 1,
+					$PlayOutMode: 3
+				},
+				
+				$BulletNavigatorOptions: {
+					$Class: $JssorBulletNavigator$,
+					$ChanceToShow: 2,
+					$AutoCenter: 1,
+					$Steps: 1,
+					$Lanes: 1,
+					$SpacingX: 8,
+					$SpacingY: 8,
+					$Orientation: 1
+				},
+				
+				$ArrowNavigatorOptions: {
+					$Class: $JssorArrowNavigator$,
+					$ChanceToShow: 1,
+					$AutoCenter: 2,
+					$Steps: 1
+				}
 			};
-
-			var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
-
-			/*#region responsive code begin*/
-
-			var MAX_WIDTH = 3000;
-
+			
+			var jssor_slider1 = new $JssorSlider$("slider1_container", options);
+			
+			//responsive code begin
 			function ScaleSlider() {
-				var containerElement = jssor_1_slider.$Elmt.parentNode;
-				var containerWidth = containerElement.clientWidth;
-
-				if (containerWidth) {
-
-					var expectedWidth = Math.min(MAX_WIDTH || containerWidth, containerWidth);
-
-					jssor_1_slider.$ScaleWidth(expectedWidth);
-				}
-				else {
-					window.setTimeout(ScaleSlider, 30);
-				}
+				var bodyWidth = document.body.clientWidth;
+				if (bodyWidth)
+				jssor_slider1.$ScaleWidth(Math.min(bodyWidth, 1920));
+				else
+				window.setTimeout(ScaleSlider, 30);
 			}
-
+			
 			ScaleSlider();
-
-			$Jssor$.$AddEvent(window, "load", ScaleSlider);
-			$Jssor$.$AddEvent(window, "resize", ScaleSlider);
-			$Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
-			/*#endregion responsive code end*/
-		};
+			
+			if (!navigator.userAgent.match(/(iPhone|iPod|iPad|BlackBerry|IEMobile)/)) {
+				$(window).bind('resize', ScaleSlider);
+			}
+			//responsive code end
+		});
 	</script>
-	<style>
-		/*jssor slider bullet skin 132 css*/
-		.jssorb132 {position:absolute;}
-		.jssorb132 .i {position:absolute;cursor:pointer;}
-		.jssorb132 .i .b {fill:#fff;fill-opacity:0.8;stroke:#000;stroke-width:1600;stroke-miterlimit:10;stroke-opacity:0.7;}
-		.jssorb132 .i:hover .b {fill:#000;fill-opacity:.7;stroke:#fff;stroke-width:2000;stroke-opacity:0.8;}
-		.jssorb132 .iav .b {fill:#000;stroke:#fff;stroke-width:2400;fill-opacity:0.8;stroke-opacity:1;}
-		.jssorb132 .i.idn {opacity:0.3;}
-
-		/*jssor slider arrow skin 051 css*/
-		.jssora051 {display:block;position:absolute;cursor:pointer;}
-		.jssora051 .a {fill:none;stroke:#fff;stroke-width:360;stroke-miterlimit:10;}
-		.jssora051:hover {opacity:.8;}
-		.jssora051.jssora051dn {opacity:.5;}
-		.jssora051.jssora051ds {opacity:.3;pointer-events:none;}
-		
-		.learnmore {padding:10px 20px;border-radius:3px;background:#E16E00;color:#fff;}
-		.learnmore:hover {background:#004E81;}
-		
-		.remarks {padding:0;}
-	</style>
-	<div id="jssor_1" style="position:relative;margin:0 auto;top:0px;left:0px;width:1920px;height:800px;overflow:hidden;visibility:hidden;">
-		<div data-u="slides" style="cursor:default;position:relative;top:0px;left:0px;width:1920px;height:800px;overflow:hidden;">
+	<div id="slider1_container" style="position:relative;margin:0 auto;top:0px;left:0px;width:1920px;height:800px;overflow:hidden;">
+		<div u="loading" style="position:absolute;top:0px;left:0px;">
+			<div style="filter:alpha(opacity=70);opacity:0.7;position:absolute;display:block;top:0px;left:0px;width:100%;height:100%;">
+			</div>
+			<div style="position:absolute;display:block;background:url(assets/images/processing.gif) no-repeat center center;top:0px;left:0px;width:100%;height:100%;">
+			</div>
+		</div>
+		<div u="slides" style="cursor:move;position:absolute;left:0px;top:0px;width:1920px;height:800px;overflow:hidden;">
 			<?php
-				$sdb = $koneksi->query("SELECT * FROM slideshow WHERE slideshow_status = 'Aktif' ORDER BY slideshow_order ASC");
-				while($sdt = $sdb->fetch_array()){
-			?>	  
+				$slidb = mysqli_query($koneksi, "SELECT * FROM slideshow ORDER BY slideshow_order ASC");
+				while($slidt = mysqli_fetch_array($slidb)) {
+			?>	
 				<div>
-					<img data-u="image" src="assets/images/slideshow/<?= $sdt['slideshow_image']; ?>" alt="<?= $sdt['slideshow_judul']; ?>" title="<?= $sdt['slideshow_judul']; ?>"/>
-					<div class="imgslide">
-						<?= $sdt['slideshow_judul']; ?>
-					</div>
+					<img u="image" class="imgbright" src="assets/images/slideshow/<?php echo $slidt['slideshow_image'];?>" alt="<?php echo $slidt['slideshow_judul'];?>" title="<?php echo $slidt['slideshow_judul'];?>">
+						<div style="position:absolute;width:50%;height:auto;top:340px;padding:0;text-align:center;line-height:auto;text-transform:uppercase;font-weight:700;font-size:60px;font-family:'Poppins', sans-serif;color:#fff;left:50%;transform:translate(-50%, -10%);">
+							<?php echo $slidt['slideshow_judul'];?>
+						</div> 
 				</div>
 			<?php } ?>
 		</div>
-		<!-- Bullet Navigator -->
-		<div data-u="navigator" class="jssorb132" style="position:absolute;bottom:24px;right:16px;" data-autocenter="1" data-scale="0.5" data-scale-bottom="0.75">
-			<div data-u="prototype" class="i" style="width:12px;height:12px;">
-				<svg viewbox="0 0 16000 16000" style="position:absolute;top:0;left:0;width:100%;height:100%;">
-					<circle class="b" cx="8000" cy="8000" r="5800"></circle>
-				</svg>
-			</div>
+		
+		<style>
+			.jssorb21 div, .jssorb21 div:hover, .jssorb21 .av {background:url(assets/images/b17.png) no-repeat;overflow:hidden;cursor:pointer;}
+			.jssorb21 div { background-position:-5px -5px;}
+			.jssorb21 div:hover, .jssorb21 .av:hover { background-position:-35px -5px;}
+			.jssorb21 .av { background-position:-65px -5px;}
+			.jssorb21 .dn, .jssorb21 .dn:hover { background-position:-95px -5px;}
+		</style>
+		<div u="navigator" class="jssorb21" style="position:absolute;bottom:120px;left:6px;">
+			<div u="prototype" style="position:absolute;width:19px;height:19px;text-align:center;line-height:19px;color:white;font-size:12px;"></div>
 		</div>
-		<!-- Arrow Navigator -->
-		<div data-u="arrowleft" class="jssora051" style="width:55px;height:55px;top:0px;left:25px;" data-autocenter="2" data-scale="0.75" data-scale-left="0.75">
-			<svg viewbox="0 0 16000 16000" style="position:absolute;top:0;left:0;width:100%;height:100%;">
-				<polyline class="a" points="11040,1920 4960,8000 11040,14080 "></polyline>
-			</svg>
-		</div>
-		<div data-u="arrowright" class="jssora051" style="width:55px;height:55px;top:0px;right:25px;" data-autocenter="2" data-scale="0.75" data-scale-right="0.75">
-			<svg viewbox="0 0 16000 16000" style="position:absolute;top:0;left:0;width:100%;height:100%;">
-				<polyline class="a" points="4960,1920 11040,8000 4960,14080 "></polyline>
-			</svg>
-		</div>
+		<style>
+			.jssora21l, .jssora21r, .jssora21ldn, .jssora21rdn {position:absolute;cursor:pointer;display:block;background:url(assets/images/a17.png) center center no-repeat;overflow:hidden;}
+			.jssora21l { background-position:-3px -33px;}
+			.jssora21r { background-position:-63px -33px;}
+			.jssora21l:hover { background-position:-123px -33px;}
+			.jssora21r:hover { background-position:-183px -33px;}
+			.jssora21ldn { background-position:-243px -33px;}
+			.jssora21rdn { background-position:-303px -33px;}
+		</style>
+		<span u="arrowleft" class="jssora21l" style="width:55px;height:55px;top:123px;left:8px;"></span>
+		<span u="arrowright" class="jssora21r" style="width:55px;height:55px;top:123px;right:8px"></span>
+		<section class="section2">
+			<div class="wave wave1"></div>
+			<div class="wave wave2"></div>
+			<div class="wave wave3"></div>
+			<div class="wave wave4"></div>
+		</section>
 	</div>
-	<script type="text/javascript">jssor_1_slider_init();</script>
-	<!-- <svg xmlns="http://www.w3.org/2000/svg" class="waves" viewBox="0 0 1440 320"><path fill="#fff" fill-opacity="1" d="M0,320L48,304C96,288,192,256,288,229.3C384,203,480,181,576,181.3C672,181,768,203,864,213.3C960,224,1056,224,1152,218.7C1248,213,1344,203,1392,197.3L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg> -->
-	<!-- <svg xmlns="http://www.w3.org/2000/svg" class="waves" viewBox="0 0 1440 320"><path fill="#fff" fill-opacity="1" d="M0,192L48,181.3C96,171,192,149,288,154.7C384,160,480,192,576,208C672,224,768,224,864,229.3C960,235,1056,245,1152,224C1248,203,1344,149,1392,122.7L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg> -->
 </div>
