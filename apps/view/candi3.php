26f759c14d2f88d78d034649bcdf340c6b6e0eb2
Dicky Darmawan <63191799+dickyd2909@users.noreply.github.com>
2023-06-23 09:46:27 +0700

    menambahkan embed youtube

--- apps/view/candi3.php
+++ apps/view/candi3.php
@@ -4,20 +4,26 @@
     <div class="carousel">
         <?php
             $no = 1;
-            $db = $koneksi->query("SELECT * FROM content WHERE contentcat_id ='CC006' AND content_id != 'CO009'");
+            $db = $koneksi->query("SELECT * FROM content WHERE contentcat_id ='CC006' AND content_id != 'CO009' OR contentcat_id = 'CC009' ");
             while($dt = $db->fetch_array()){
         ?>
-            <a href="#<?= $no++; ?>" class="carousel-item">
-                <img src="assets/images/content/<?= $dt['content_image']; ?>" alt="<?= $dt['content_judul']; ?>" title="<?= $dt['content_judul']; ?>">
-                <div class="carousel-info">
-                    <div class="carousel-info-tit">
-                        <?= $dt['content_judul']; ?>
+            <?php if($dt['contentcat_id'] == 'CC006'){ ?>
+                <a href="#<?= $no++; ?>" class="carousel-item">
+                    <img src="assets/images/content/<?= $dt['content_image']; ?>" alt="<?= $dt['content_judul']; ?>" title="<?= $dt['content_judul']; ?>">
+                    <div class="carousel-info">
+                        <div class="carousel-info-tit">
+                            <?= $dt['content_judul']; ?>
+                        </div>
+                        <div class="carousel-info-desc">
+                            <?= $dt['content_desc']; ?>
+                        </div>
                     </div>
-                    <div class="carousel-info-desc">
-                        <?= $dt['content_desc']; ?>
-                    </div>
-                </div>
-            </a>
+                </a>
+            <?php }else{ ?>
+                <a href="#<?= $no++; ?>" class="carousel-item">
+                    <iframe width="600" height="480" src="<?= $dt['content_url']; ?>" allow="autoplay"></iframe>
+                </a>
+            <?php } ?>    
         <?php } ?>
     </div>
     <div class="btn-carousel-box">
